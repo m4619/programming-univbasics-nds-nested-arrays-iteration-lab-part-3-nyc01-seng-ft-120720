0553f4bc7c4c9ab46a857a7ec2e45da193718d44
Miguel  Arreola <marreola4619@gmail.com>
2020-11-13 20:54:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do 
-      if src[row_index][element_index].is_a? 
+      if src[row_index][element_index].is_a? String  
         new_array << src[row_index][element_index]
       end
       element_index += 1 
